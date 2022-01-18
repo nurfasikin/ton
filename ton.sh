f41e287e8462c54b26303db27737ace754446edc
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-19 01:07:06 +0700

    Create ton.sh

---
+++ ton.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz
+tar xvf lolMiner_v1.41b_Lin64.tar.gz
+mv 1.41b mng 
+cd mng
+chmod +x lolMiner 
+./lolMiner --algo TON --pool https://pool.services.tonwhales.com --user EQCzFTXpNNsFu8IgJnRnkDyBCL2ry8KgZYiDi3Jt31ie8EIQ

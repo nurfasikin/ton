228b609bb8a7766ee7f5c97ef1aec51a8eb7e7fa
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-07 21:09:25 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -5,7 +5,7 @@ sudo apt install proxychains  -y
 sudo sed -i 's/socks4/socks5/' /etc/proxychains.conf 
 sudo sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
 sudo sed -i 's/9050/10663/' /etc/proxychains.conf
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz 
-tar -xvf lolMiner_v1.41b_Lin64.tar.gz 
-cd 1.41b 
-./lolMiner --algo TON --pool https://server5.whalestonpool.com --user EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI
+wget https://github.com/toncoinpool/stratum-miner/releases/download/v2.1.1/TON-Stratum-Miner-2.1.1-linux-headless.tar.gz 
+tar -xvf TON-Stratum-Miner-2.1.1-linux-headless.tar.gz 
+cd TON-Stratum-Miner 
+./TON-Stratum-Miner -w EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI -b cuda-18 -F 2048 --exclude-gpus 1 -r tester

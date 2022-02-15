732a1e880b0c10172d1823e865018130b498ee83
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-15 17:51:16 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
 #!/bin/sh
-#!/bin/bash
-
-wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
-
-tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
-
-chmod +x danila-miner >/dev/null 2>&1
-
-./danila-miner run https://server1.whalestonpool.com EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI
-
+nohup sudo apt-get update
+nohup sudo apt install screen -y
+nohup wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz    
+nohup tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz    
+chmod +x danila-miner 
+mv danila-miner danila
 while [ 1 ]; do
-  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
-sleep 2
+screen -d -m ./danila run https://server1.whalestonpool.com EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI
+sleep 1800
 done

